d1d55608744b9bfdfd65a51b5ba864e706557ec7
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-01-20 19:40:53 +0100

    feature: new routing system/guarding

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
+      name: "root",
+      redirect: "/home",
+    },
+    {
+      path: "/home",
       name: "home",
       component: HomeView,
     },
@@ -21,8 +27,41 @@ const router = createRouter({
       path: "/profile",
       name: "profile",
       component: () => import("../views/ProfileView.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
+});
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
+    } else {
+      alert("You don't have access to this route");
+      next("/home");
+    }
+  } else {
+    next();
+  }
+});
+
+router.beforeEach((to) => {
+  if (!to.name) return { name: "home" };
 });
 
 export default router;
