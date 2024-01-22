7f555934a2edcd729ab1c0985fa45eb9b09bf822
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-01-22 19:59:05 +0100

    refactor: refactor quiz and results into a parent component

---
+++ src/components/Home Components/QuizAndResults.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import QuizComponent from '@/components/Home Components/QuizComponent.vue';
+import { useQuizStore } from '@/stores/quiz';
+import ResultsComponent from './ResultsComponent.vue';
+
+const quizStore = useQuizStore()
+</script>
+        
+<template>
+    <QuizComponent :questions="quizStore.questions" />
+    <ResultsComponent :questions="quizStore.questions" />
+</template>
+
+<style scoped></style>
