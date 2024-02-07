2b041de85132642039f972ba42db2e6d542a3ee7
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-02-07 21:52:03 +0100

    new: reusable loader component for pages that use suspense api

---
+++ src/components/Loader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = defineProps({
+    text: {
+        type: String,
+        required: true,
+    }
+})
+</script>
+        
+<template>
+    <div class="loader">
+        <span>{{ props.text }}</span>
+    </div>
+</template>
+
+
+<style scoped>
+@keyframes bounce {
+
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+        transform: translateY(0);
+    }
+
+    40% {
+        transform: translateY(-10px);
+    }
+
+    60% {
+        transform: translateY(-5px);
+    }
+}
+
+.loader {
+    display: grid;
+    min-height: 80svh;
+    place-items: center;
+    animation: bounce 1s ease infinite;
+
+}
+</style>
