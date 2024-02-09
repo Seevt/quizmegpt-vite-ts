390da2d0a2973256c92bb2b2012d169cbd23e6cd
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-02-09 16:17:43 +0100

    feature: login form validation using zod

--- src/components/Login Components/Login.vue
+++ src/components/Login Components/Login.vue
@@ -1,47 +1,86 @@
 <script setup lang="ts">
-import { getAuth, signInWithEmailAndPassword, type Auth, type UserCredential } from "firebase/auth";
 import { ref } from "vue";
 import { useRouter } from 'vue-router';
+import { getAuth, signInWithEmailAndPassword, type Auth, type UserCredential } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+import * as z from 'zod';
 
 const router = useRouter();
+
+const formSchema = z.object({
+    email: z.string().min(1, { message: "You must specify an email" }).email({ message: "Please enter a valid email address" }),
+    password: z.string().min(1, { message: "You must enter a password" }),
+})
 
 const login_form = ref({
     email: "",
     password: "",
 })
 
+const emailError = ref<string | null>(null);
+const passwordError = ref<string | null>(null);
+const firebaseError = ref<string | null>(null)
+
+
+function validateField(fieldName: keyof typeof formSchema.shape) {
+    try {
+        formSchema.shape[fieldName].parse(login_form.value[fieldName]);
+        (fieldName === 'email' ? emailError : passwordError).value = null
+    } catch (error: any) {
+        (fieldName === 'email' ? emailError : passwordError).value = error.issues[0].message
+        console.log(error.issues);
+    }
+}
+
 function login(): void {
     const auth: Auth = getAuth()
+
+    if (emailError.value || passwordError.value) return
+
     signInWithEmailAndPassword(
         auth,
         login_form.value.email, login_form.value.password
     )
         .then((data: UserCredential) => {
-            console.log("Sucessfully logged in");
-            console.log(auth.currentUser);
-
             router.push('/profile')
         })
         .catch((error: any) => {
+
+            if (error instanceof FirebaseError) {
+                firebaseError.value = error.code;
+            }
+            firebaseError.value = "The username or password you entered is incorrect. Please try again"
             console.log(error.code);
-            alert(error.message)
+            // alert(error.message)
         })
 }
+
 </script>
 
 <template>
     <h1>Sign In</h1>
     <form @submit.prevent="login" class="form">
         <label for="email">Email</label>
-        <input name="email" placeholder="Enter your email" type="email" id="email" v-model="login_form.email" />
+        <input @blur="validateField('email')" name="email" :class="{ 'active-error': emailError }"
+            placeholder="Enter your email" id="email" v-model="login_form.email" />
+
+        <span role="error message" v-if="emailError" class="zod-error">{{ emailError }}</span>
+
 
         <label for="password">Password</label>
-        <input name="password" type="password" placeholder="Enter your password" id="password"
-            v-model="login_form.password" />
+        <input @blur="validateField('password')" name="password" :class="{ 'active-error': passwordError }" type="password"
+            placeholder="Enter your password" id="password" v-model="login_form.password" />
+
+        <span role="error message" v-if="passwordError" class="zod-error">{{ passwordError }}</span>
+
         <button class="submit-button" type="submit">
             <p>Continue</p>
             <p> {{ "➜" }}</p>
         </button>
+
+        <div v-if="firebaseError" class="firebase-error">
+            <span role="error message">{{ firebaseError }}</span>
+        </div>
     </form>
 </template>
 
@@ -70,6 +109,35 @@ label {
     margin-bottom: 0.5rem;
     margin-left: .5rem;
 
+
+}
+
+.zod-error {
+    display: flex;
+    flex-direction: column;
+    margin-top: 5px;
+    margin-left: .8rem;
+    margin-bottom: 20px;
+    font-size: calc(var(--p-size)/1.3);
+    color: var(--error-color);
+    font-weight: 400;
+    transition: all;
+}
+
+.firebase-error {
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+    margin-left: .8rem;
+    margin-bottom: 20px;
+    font-size: calc(var(--p-size)/1.25);
+    color: var(--error-color);
+    font-weight: 500;
+    /* max-width: 350px; */
+}
+
+.firebase-error span {
+    max-width: 350px;
 }
 
 input {
@@ -82,6 +150,20 @@ input {
     font-size: medium;
     border: 1px solid hsl(0, 0%, 86%);
 }
+
+input.active-error {
+    border: 1px solid var(--error-color);
+}
+
+
+.form input.active-error {
+    margin-bottom: 0;
+}
+
+.form input:last-of-type.active-error {
+    margin-bottom: 0;
+}
+
 
 input::placeholder {
     opacity: 1;
@@ -144,3 +226,5 @@ input:focus {
     transition: all 0.3s ease-in-out;
 }
 </style>
+
+ 
