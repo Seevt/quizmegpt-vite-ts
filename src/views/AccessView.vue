8bee97e9d22e51899605b4378768e6bfd021ad89
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-02-26 20:29:56 +0100

    rename  + query component persistence

---
+++ src/views/AccessView.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+import Login from '@/components/Login Components/Login.vue';
+import Register from '@/components/Login Components/Register.vue';
+import { getAuth, sendPasswordResetEmail, type Auth } from 'firebase/auth';
+import { useRouter, useRoute } from 'vue-router';
+
+import { computed, ref } from 'vue';
+
+const route = useRoute()
+const router = useRouter()
+const componentQuery = computed(() => {
+  return route.query.type === "register" ? Register : Login;
+})
+
+function changeForm() {
+  if (route.query.type === "register") {
+    router.push({
+      query: {
+        type: "login"
+      }
+    })
+  } else {
+    router.push({
+      query: {
+        type: "register"
+      }
+    })
+
+  }
+}
+
+console.log(componentQuery.value, route.query.type);
+
+let auth: Auth;
+async function resetPassword(): Promise<void> {
+  auth = getAuth();
+  let emailToReset = prompt("Please enter your email:");
+  if (emailToReset) {
+    try {
+      await sendPasswordResetEmail(auth, emailToReset);
+      alert("Password reset email!");
+    } catch (error: any) {
+      alert("Failed to send password reset email: " + error.message);
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="login">
+    <header>
+      <RouterLink to="/">
+        <img class="logo" loading="lazy" src="../assets/imgs/quizmegpt-logo2.png" />
+      </RouterLink>
+    </header>
+
+    <div class="container">
+      <div class="content">
+        <component :is="componentQuery" />
+        <nav role="controls" class="options-wrapper">
+          <button @click="changeForm" class="toggle-button">
+            {{
+              route.query.type === 'register' ? "Already have an account? Log In" : "Create account"
+            }}
+          </button>
+          <button v-if="route.query.type !== 'register'" class="toggle-button" @click.prevent="resetPassword">
+            Forgot password?
+          </button>
+        </nav>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+* {
+  --form-text-weight: 500;
+  --logo-height: 79px;
+}
+
+/* main container for the view */
+.login {
+  /* flex-grow was there before min-h to ensure it grew, but min-h is more realiable */
+  position: relative;
+  min-height: 100svh;
+  flex-grow: 1;
+  display: flex;
+  --spacing: clamp(1rem, 5%, 2rem);
+  padding-bottom: var(--spacing);
+  flex-direction: column;
+  background: linear-gradient(rgba(0, 0, 0, 0.2),
+      rgba(0, 0, 0, 0.15)),
+    url("../assets/imgs/logo_bg.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+@media (max-height: 640px) or (max-width: 500px) {
+  .login {
+    gap: var(--spacing);
+  }
+}
+
+/* logo */
+.logo {
+  position: relative;
+  padding-top: 1rem;
+  object-fit: contain;
+  width: 70px;
+  height: var(--logo-height);
+  inset: 0;
+  margin: 0 auto;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  padding: 0;
+  width: 100%;
+}
+
+
+.content {
+  background-color: hsl(0, 0%, 96%);
+  border-radius: var(--card-radius);
+  box-shadow: 0 0 10px rgba(19, 19, 19, 0.1);
+  padding: clamp(1rem, 5%, 2rem);
+  padding-block: var(--padding-y);
+  padding-inline: var(--padding-x);
+  width: min(95%, 500px);
+  font-weight: var(--form-text-weight);
+}
+
+.options-wrapper {
+  margin-top: 1rem;
+  display: grid;
+}
+
+.toggle-button {
+  padding-bottom: 0;
+  background-color: transparent;
+  color: rgb(46, 46, 46);
+  border: none;
+  cursor: pointer;
+}
+
+.toggle-button:hover {
+  color: #080808;
+}
+</style>
